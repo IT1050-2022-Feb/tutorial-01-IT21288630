c9a6184ed704ac125c27a2c3ddb72307911c4afa
IT21288630 <99311971+IT21288630@users.noreply.github.com>
2022-02-09 06:07:14 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -10,14 +10,17 @@ int main()
   int mark1, mark2;
   float avg = 0;
   
+  //get two marks from keyboard
   printf("Enter mark1 : ");
   scanf("%d", &mark1);
   
   printf("Enter mark2 : ");
   scanf("%d", &mark2);
 
+  //Calculating average
   avg = (mark1 + mark2) / 2;
 
+  //printing the output
   printf("\nAverage = %.2f\n", avg);
 
   return 0;
